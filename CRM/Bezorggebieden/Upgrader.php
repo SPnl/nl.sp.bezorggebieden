9c02106e2d94242df390f43689d530e3c2326198
Jaap Jansma <jaap@edeveloper.nl>
2015-03-07 14:49:40 +0100

    added auto fill to the upgrader

--- CRM/Bezorggebieden/Upgrader.php
+++ CRM/Bezorggebieden/Upgrader.php
@@ -14,4 +14,25 @@ class CRM_Bezorggebieden_Upgrader extends CRM_Bezorggebieden_Upgrader_Base {
     $this->executeCustomDataFile('xml/contact_bezorggebied.xml');
   }
 
+  public function upgrade_1002() {
+    $count = CRM_Core_DAO::singleValueQuery('SELECT count(*) FROM civicrm_contact where contact_type = "Individual"');
+    for ($startId = 0; $startId <= $count; $startId += 250) {
+      $title = ts('Correct addresses (%1 / %2)', array(
+        1 => $startId,
+        2 => $count,
+      ));
+      $this->addTask($title, 'update_address');
+    }
+
+    civicrm_api3('BezorggebiedContact', 'update', array('force' => 1));
+
+    return true;
+  }
+
+  public static function update_address() {
+    civicrm_api3('BezorggebiedContact', 'update', array('force' => 1));
+
+    return true;
+  }
+
 }
